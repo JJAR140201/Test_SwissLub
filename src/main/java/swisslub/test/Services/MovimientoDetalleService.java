683925771c26a1c4dd2318a365646bffc739cade
JJAR140201 <avilesroajuanjose@gmail.com>
2023-08-12 11:32:09 -0500

    Creamos los servicios que son interfaces que se extienden de CrudService

---
+++ src/main/java/swisslub/test/Services/MovimientoDetalleService.java
@@ -0,0 +1,6 @@
+package swisslub.test.Services;
+
+import swisslub.test.DTO.MovimientoDetalle;
+
+public interface MovimientoDetalleService extends CrudService<MovimientoDetalle> {
+}
---
+++ src/main/java/swisslub/test/Services/MovimientoService.java
@@ -0,0 +1,6 @@
+package swisslub.test.Services;
+
+import swisslub.test.DTO.MovimientosDTO;
+
+public interface MovimientoService extends CrudService<MovimientosDTO> {
+}
