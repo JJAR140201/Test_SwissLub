0db0f209cb5e9ffff95a5b3d41d6c08e26b5deb8
JJAR140201 <avilesroajuanjose@gmail.com>
2023-08-12 13:08:01 -0500

    Se mapearon bien los datos donde se puede hacer bien las relaciones

--- src/main/java/swisslub/test/DTO/BodegaDTO.java
+++ src/main/java/swisslub/test/DTO/BodegaDTO.java
@@ -1,18 +1,28 @@
-package swisslub.test.DTO;
+    package swisslub.test.DTO;
 
-import lombok.*;
+    import lombok.*;
 
-import javax.persistence.*;
+    import javax.persistence.*;
+    import java.util.List;
 
-@Entity
-@Table(name = "bodega")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-public class BodegaDTO {
-    @Id
-    private String codigo;
-    private String nombre;
-}
+    @Entity
+    @Table(name = "bodega")
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @ToString
+    public class BodegaDTO {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+
+        private String codigo;
+        private String nombre;
+
+        @OneToMany(mappedBy = "bodegaOrigen")
+        private List<MovimientosDTO> movimientosOrigenList;
+
+        @OneToMany(mappedBy = "bodegaDestino")
+        private List<MovimientosDTO> movimientosDestinoList;
+    }
