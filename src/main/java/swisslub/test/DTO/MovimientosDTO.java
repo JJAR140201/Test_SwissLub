17b2828d5197f74a4badafea692efaaccae3288c
JJAR140201 <avilesroajuanjose@gmail.com>
2023-08-12 11:14:50 -0500

    Creamos las relaciones en el dto MovimientosDTO

---
+++ src/main/java/swisslub/test/DTO/MovimientosDTO.java
@@ -0,0 +1,35 @@
+package swisslub.test.DTO;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "movimiento")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MovimientosDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "id_empresa")
+    private EmpresaDTO empresa;
+    private String descripcion;
+    @ManyToOne
+    @JoinColumn(name = "bodega_origen_codigo")
+    private BodegaDTO bodegaOrigen;
+    @ManyToOne
+    @JoinColumn(name = "bodega_destino_codigo")
+    private BodegaDTO bodegaDestino;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fecha_creacion")
+    private Data fechaCreacion;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fecha_entrega")
+    private Data fechaEntrega;
+    private char estado;
+}
