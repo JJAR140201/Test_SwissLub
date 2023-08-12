11923994f88fa8f5f2f1622eb47c236965052b78
JJAR140201 <avilesroajuanjose@gmail.com>
2023-08-12 11:31:25 -0500

    Se creo el CrudService para los metodos de busqueda, agregar, editar y eliminar

---
+++ src/main/java/swisslub/test/Services/CrudService.java
@@ -0,0 +1,15 @@
+package swisslub.test.Services;
+
+import java.util.List;
+
+public interface CrudService<T> {
+    public List<T> listarTodos();
+
+    public T listarPorId(int id);
+
+    public void registrar(T entidad);
+
+    public void actualizar(T entidad);
+
+    public void eliminar(int id);
+}
