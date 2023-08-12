d8b439a350f891f53a9abd58d0b68ef26719279f
JJAR140201 <avilesroajuanjose@gmail.com>
2023-08-12 11:16:41 -0500

    Creamos el DTO MovimientoDetalle

---
+++ src/main/java/swisslub/test/DTO/MovimientoDetalle.java
@@ -0,0 +1,18 @@
+package swisslub.test.DTO;
+import lombok.*;
+
+import javax.persistence.*;
+@Entity
+@Table(name = "movimiento_detalle")
+public class MovimientoDetalle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "movimiento_id")
+    private MovimientosDTO movimiento;
+    @Column(name = "item_codigo")
+    private String itemCodigo;
+    @Column(name = "cantidad_enviada")
+    private int cantidadEnviada;
+}
