200e87b32a9c91e4908d5062bdf8c714a9897faa
JJAR140201 <avilesroajuanjose@gmail.com>
2023-08-13 07:59:56 -0500

    Archivos requeridos por la empresa

---
+++ src/main/resources/SQL/Inser.movimiento-detalle.sql
@@ -0,0 +1,17 @@
+USE test_swisslub;
+INSERT INTO test_swisslub.movimiento_detalle (id, cantidad_enviada, producto, movimiento_id)
+VALUES
+    (2, 100, 'Producto A', 1),
+    (3, 200, 'Producto B', 2),
+    (4, 150, 'Producto C', 3),
+    (5, 50, 'Producto A', 4),
+    (6, 75, 'Producto B', 5),
+    (7, 120, 'Producto C', 6),
+    (8, 90, 'Producto A', 7),
+    (9, 180, 'Producto B', 8),
+    (10, 60, 'Producto C', 9),
+    (11, 130, 'Producto A', 10),
+    (12, 70, 'Producto B', 11),
+    (13, 110, 'Producto C', 12),
+    (14, 95, 'Producto A', 13),
+    (15, 250, 'Producto B', 14);
---
+++ src/main/resources/SQL/Inser.movimientos.sql
@@ -0,0 +1,13 @@
+USE test_swisslub;
+INSERT INTO test_swisslub.movimiento (id, descripcion, empresa, estado, fecha_creacion, fecha_entrega)
+VALUES
+    (5, 'Movimiento 5', 'Empresa AA', 'P', '2023-08-13', '2023-08-15'),
+    (6, 'Movimiento 6', 'Empresa AB', 'T', '2023-08-14', '2023-08-16'),
+    (7, 'Movimiento 7', 'Empresa AC', 'E', '2023-08-15', '2023-08-17'),
+    (8, 'Movimiento 8', 'Empresa AD', 'P', '2023-08-16', '2023-08-18'),
+    (9, 'Movimiento 9', 'Empresa BA', 'T', '2023-08-17', '2023-08-19'),
+    (10, 'Movimiento 10', 'Empresa AB', 'E', '2023-08-18', '2023-08-20'),
+    (11, 'Movimiento 11', 'Empresa AC', 'P', '2023-08-19', '2023-08-21'),
+    (12, 'Movimiento 12', 'Empresa AD', 'T', '2023-08-20', '2023-08-22'),
+    (13, 'Movimiento 13', 'Empresa AA', 'E', '2023-08-21', '2023-08-23'),
+    (14, 'Movimiento 14', 'Empresa AB', 'P', '2023-08-22', '2023-08-24');
